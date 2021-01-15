c7a59c0a3c12dce5aba10040c44717cf4c3762e0
KaMeHb-UA <marlock@bassteam.ml>
2021-01-15 10:08:49 +0200

    describe Element as an abstract class

--- src/base.d.ts
+++ src/base.d.ts
@@ -1,3 +1,9 @@
-export class Element {
+/**
+ * The class does not represent itself, but any child.
+ * 
+ * For example, <a href="_elements_d_.page.html" class="tsd-signature-type">Page</a>
+ * @abstract
+ */
+export abstract class Element {
     type: string
 }
